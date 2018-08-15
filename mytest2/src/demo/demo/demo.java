a3c353b4d246a200b82119d43fd8808dbc2697f1
ccc <ccc@163.com>
2018-08-15 22:47:21 +0800

    ddd

--- mytest2/src/demo/demo/demo.java
+++ mytest2/src/demo/demo/demo.java
@@ -3,5 +3,7 @@ package demo.demo;
 public class demo {
   public static void main(String[] args) {
 	System.out.println("ssssssss");
+	System.out.println("ssssssss");
+	System.out.println("ssssssss");
 }
 }
